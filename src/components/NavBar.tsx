23d5834b236d2e852aa43e3d5d2c97d1c014d201
akasataikisiti <nama0you0chu@gmail.com>
2024-02-08 17:17:31 +0900

    first commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import { Link } from "wouter-preact"
+
+export default function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
