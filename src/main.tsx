0d69916d0e2db061827d812a18914166c2ed2179
akasataikisiti <nama0you0chu@gmail.com>
2024-02-08 18:48:21 +0900

    eslint調整

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
-import './index.css'
+import { render } from "preact";
+import "./css/index.css";
 
-render(<App />, document.getElementById('app')!)
+function Main() {
+  return (
+    <div>
+      <h1>My Preact App</h1>
+      <p>
+        Edit <code>src/main.tsx</code> and save to test HMR
+      </p>
+    </div>
+  );
+}
+
+render(<Main />, document.getElementById("app")!);
